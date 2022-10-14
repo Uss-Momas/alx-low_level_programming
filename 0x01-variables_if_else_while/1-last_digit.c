0fe3f471bc5c49fb094624bbf671b21b032b1afa
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-10-14 22:39:26 +0000

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 	if (n > 5)
 		printf("and is greater than 5\n");
 	else if (n == 0)
-		printf("and is zero\n");
+		printf("and is 0\n");
 	if (n < 6 && n != 0)
 		printf("and is less than 6 and not 0\n");
 	return (0);
