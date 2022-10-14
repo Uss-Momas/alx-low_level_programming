c8a207e15e099fc8a2108c5655524248ee8dbc9c
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-10-14 22:55:28 +0000

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  * Return: 0 succes
+  */
+int main(void)
+{
+	for (int ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+		if (ch == 'z')
+			putchar('\n');
+	}
+	return (0);
+}
