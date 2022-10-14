d3f25a36a6982f7bc46fb1fdc7713860e4c45f77
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-10-14 23:10:04 +0000

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  * Return: 0 sucess
+  */
+int main(void)
+{
+	int ch = 'a';
+	int upChar = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		if (ch == 'z')
+			while (upChar <= 'Z')
+			{
+				putchar(upChar);
+				upChar++;
+				if (upChar == 'Z')
+				{
+					putchar('\n');
+					break;
+				}
+			}
+		ch++;
+	}
+	return (0);
+}
