a7dd7fc36281b7b072327d2d9b58d613927297b9
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-10-19 09:45:26 +0000

    implement code to generate 24hrs in minute

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * jack_bauer - prints every min of a day
+  * Return: nothing
+  */
+void jack_bauer(void)
+{
+	for (int i = 0; i < 24; i++)
+	{
+		for (int j = 0; j < 60; j++)
+		{
+			int first = i / 10;
+			int second = i % 10;
+			int third = j / 10;
+			int fourth = j % 10;
+
+			_putchar(first + '0');
+			_putchar(second + '0');
+			_putchar(':');
+			_putchar(third + '0');
+			_putchar(fourth + '0');
+			_putchar('\n');
+		}
+	}
+}
