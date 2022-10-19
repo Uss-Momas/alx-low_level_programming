7f789d9581200340df94b68630edaedd6868b6df
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-10-19 10:31:46 +0000

    Implements the sum of 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  * add - adds two numbers
+  * @a: first number
+  * @b: second number
+  * Return: the sum of the 2 numbers
+  */
+int add(int a, int b)
+{
+	return (a + b);
+}
