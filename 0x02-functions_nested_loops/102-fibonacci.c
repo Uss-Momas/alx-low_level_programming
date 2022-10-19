279a4650547e98542275ccf616ea62935a402871
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-10-19 18:32:45 +0000

    Fixing the limit of the numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,12 +8,12 @@ int main(void)
 	long int count = 0, previous = 1, now = 2, next;
 
 	printf("%ld, %ld", previous, now);
-	while (count <= 50)
+	while (count <= 48)
 	{
 		next = previous + now;
 		previous = now;
 		now = next;
-		if (count == 50)
+		if (count == 48)
 			printf("\n");
 		else
 			printf(", %ld", next);
