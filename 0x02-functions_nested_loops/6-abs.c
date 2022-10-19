3c9415f2d97cd9b2148c22f505de3ae63d8794c8
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-10-19 08:38:20 +0000

    implement absolute integer method

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * _abs - calculates the absolute value of an integer
+  * @n: is the integer
+  * Return: the absolute value of an integer
+  */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
