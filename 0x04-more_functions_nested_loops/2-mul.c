c3e5fc66d203596f8fd89a9e54c1bd5da8f270cc
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-10-21 15:17:29 +0000

    Implementing the multiply function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  * mul - function to multiply 2 numbers
+  * @a: first number
+  * @b: second number
+  * Return: the value of the multiplication
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
