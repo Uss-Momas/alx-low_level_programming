b54487d2f8e75806f41d5db4e83977cd9fa46549
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-10-21 18:24:21 +0000

    Changing folder structure

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * main - entry point
+  * Return: 0 for success
+  */
+int main(void)
+{
+	long long int n = 612852475143;
+
+	printf("%lld \n", n);
+	return (0);
+}
