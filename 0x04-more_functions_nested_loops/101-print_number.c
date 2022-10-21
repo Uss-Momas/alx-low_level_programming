e56d85a29107a52a8dcf737f8a9fddeeecf10712
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-10-21 18:02:30 +0000

    Printing integer numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * print_number - imprime um integer;
+  * @n: o inteiro
+  * Return: nothing
+  */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10 != 0)
+		print_number(n / 10);
+
+	_putchar(n % 10 + '0');
+}
