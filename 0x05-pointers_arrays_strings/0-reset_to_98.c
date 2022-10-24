2af1ea059f947897922a596922b387b025170e2a
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-10-24 20:58:39 +0000

    Function to reset a variable number

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+  * reset_to_98 - resets a value back to 98
+  * @n: is the pointer of the address of the variable that we want to modify
+  * Return: nothing
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+# 0. Pointers in C
