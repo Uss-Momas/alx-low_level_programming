9e1a6d2c4ec0ca229df716be88a8055d8394e658
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-10-24 21:22:41 +0000

    Fun to return the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+  * _strlen - function thar returns the length of a string
+  * @s: is the String
+  * Return: the length of the @s
+  */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
