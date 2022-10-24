375eb543513bba39011da9d48a7c05bcc17418ea
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-10-24 21:33:25 +0000

    function to print a String message

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * _puts - prints the string in the stdout
+  * @str: is the string value
+  * Return: nothing
+  */
+void _puts(char *str)
+{
+	puts(str);
+}
