9e1a8abe0d63d549f12c3cb76f7a54e1c294f33f
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-10-25 01:01:03 +0000

    Fixing overflow

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 /**
   * _atoi - converts a string to an integer
   * @s: the string value
@@ -6,14 +7,29 @@
   */
 int _atoi(char *s)
 {
-	int res = 0, i, sign = 1, countNegatives = 0;
+	int res = 0, i = 0, sign = 1, countNegatives = 0;
 
-	for (i = 0; s[i] != '\0'; ++i)
+	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 			countNegatives++;
-		else if (s[i] >= 48 && s[i] <= 57)
+		i++;
+	}
+	
+	if (countNegatives % 2 != 0)
+		sign = -1;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		if (s[i] >= 48 && s[i] <= 57)
 		{
+			if (res > INT_MAX / 10 || (res == INT_MAX / 10 && s[i] - '0' > 7))
+			{
+				if (sign == 1)
+					return INT_MAX;
+				else
+					return INT_MIN;
+			}
 			res = res * 10 + s[i] - '0';
 			if (s[i + 1] != '\0')
 				if (!(s[i + 1] >= 48 && s[i + 1] <= 57))
@@ -21,7 +37,5 @@ int _atoi(char *s)
 		}
 	}
 
-	if (countNegatives % 2 != 0)
-		sign = -1;
 	return (res * sign);
 }
