b88a1218f96b21defedade4cbd943035d9ba74e1
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-10-27 14:19:52 +0000

    Modifications

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,26 +4,15 @@
   * @dest: is the destiny of the new content
   * @src: is the string to be copied
   * @n: is the number of chars to be copied
-  * Return: the dest
+  * Return: the pointer to the dest
   */
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *p = dest;
-	int i = 0;
+	int i;
 
-	while (*src != '\0' && i < n)
-	{
-		*p = *src;
-		p++;
-		src++;
-		i++;
-	}
-
-	while (i < n)
-	{
-		*p = '\0';
-		p++;
-	}
-
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
