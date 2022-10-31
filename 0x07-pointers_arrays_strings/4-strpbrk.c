9f48492b1838d3f3165dd8c9b351d41286aefda1
Uss-Momas <ussumane@Ussumane-Momade.localdomain>
2022-11-01 00:10:29 +0200

    Task 4 Fixing Betty style

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,9 +11,9 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i = 0, j;
 
-	for(; s[i] != '\0'; i++)
+	for (; s[i] != '\0'; i++)
 	{
-		for(j = 0; accept[j] != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
