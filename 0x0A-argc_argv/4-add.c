6d1202b7d96420bfccbaa63b6fc6419176a63d1a
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-11-05 00:16:52 +0200

    code to sum numbers comming as argumnets

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - entry point of the program
+  * @argc: the number os arguments of the program
+  * @argv: is a pointer to a pointer of strings
+  * Return: 0 if it's success
+  */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			if (*argv[i] >= 'a' && *argv[i] <= 'z')
+			{
+				printf("Error\n");
+				return (-1);
+			}
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+	else if (argc == 1)
+		printf("0\n");
+	return (0);
+}
