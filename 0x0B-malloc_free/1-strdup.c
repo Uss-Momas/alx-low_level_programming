79183d93ef5ce6dc0f9ab4c72c143eae922a3282
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-11-08 19:04:21 +0200

    func to copy a string to another alocated space

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+  * _strdup - returns a pointer to a new string that
+  *  is a copy of a given string as param
+  * @str: the string given as parameter
+  * Return: the pointer to the alocated space or NULL
+  */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+	ptr = malloc(sizeof(*str) * strlen(str));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i <= 5; i++)
+	{
+		ptr[i] = str[i];
+	}
+	return (ptr);
+
+}
