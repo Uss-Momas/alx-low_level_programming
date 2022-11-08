43f130a752b801fbbbd431a05921dc97b732f142
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-11-08 21:12:42 +0200

    function that frees memory of a 2D array

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * free_grid - frees the mem alocated to a 2D array
+  * @grid: the 2D array
+  * @height: the number of lines
+  * Return: nothing
+  */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
