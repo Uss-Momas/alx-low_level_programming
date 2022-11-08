6d2792afa9c9b883087d45556f54f8b9fd8cac5e
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-11-08 21:04:59 +0200

    fixing

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,12 +24,12 @@ int **alloc_grid(int width, int height)
 		ptr[i] = (int *)malloc(sizeof(int *) * width);
 		if (ptr[i] == NULL)
 		{
-			for(i--; i >= 0; i--)
+			for (i--; i >= 0; i--)
 				free(ptr[i]);
+			free(ptr);
 			return (NULL);
 		}
 	}
-
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			ptr[i][j] = 0;
