34b35fb57a23a255980a8a24b83ef1f06838606d
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-11-08 20:33:41 +0200

    function to alloc mem to a 2D array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+/**
+  * alloc_grid - allocates space for a 2D array
+  * @width: the number of columns
+  * @height: the number of lines
+  * Return: the 2D pointer
+  */
+int **alloc_grid(int width, int height)
+{
+	int **ptr, i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	ptr = malloc(sizeof(int) * height);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = (int *) malloc(sizeof(int) * width);
+	}
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			ptr[i][j] = 0;
+	return (ptr);
+}
