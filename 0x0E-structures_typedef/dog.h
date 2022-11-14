17aeb4ed7fa195575cf78c8633fae37fb6425efe
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-11-14 23:41:13 +0200

    Implementing Dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef __DOG_H__
+#define __DOG_H__
+
+/**
+ * struct dog - dog type
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: this structure defines how a dog type could be
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
