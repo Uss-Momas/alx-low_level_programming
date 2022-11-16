102f700d89c9eaab98d0a670b929152fa28834f5
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-11-16 21:00:26 +0200

    Code to print a name!

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+  * print_name - function that prints a name
+  * @name: is the name to be printed;
+  * @f: is a pointer to a function with no return
+  * that takes a pointer to a char as parameter
+  * Return: nothing
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
