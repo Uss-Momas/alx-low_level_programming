a2d2a013b5c49a4d8cd14a8a594b97370db95c86
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-11-27 21:44:42 +0200

    Function that returns Length of a singly list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,7 +15,7 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		h = (const list_t *) h->next;
+		h = h->next;
 		tot++;
 	}
 	return (tot);
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,6 +9,11 @@ size_t list_len(const list_t *h)
 	size_t len;
 
 	len = 0;
+	while (h!= NULL)
+	{
+		h = h->next;
+		len++;
+	}
 
 	return (len);
 }
