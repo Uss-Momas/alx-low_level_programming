fdc31eab808d972e23a73a5794caeb8949b8c92f
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-11-27 21:35:47 +0200

    Fixing betty style

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 /**
-  *
-  *
+  * list_len - function that calculates the number of elements
+  * @h: the singly linked lists
+  * Return: the length of the singly linked lists
   */
 size_t list_len(const list_t *h)
 {
