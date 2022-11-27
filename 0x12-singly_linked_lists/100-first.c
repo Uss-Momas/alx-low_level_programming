8c97a841a97fe5d9f165e79b3a11a3fc381cd361
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-11-27 23:48:54 +0200

    Function that prints message before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+  * premain - a special fun that runs before main
+  * Return: nothing
+  */
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
