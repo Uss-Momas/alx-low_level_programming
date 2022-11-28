3ca4dfdeb0377b0907d3f33decd5d2c00cf5356e
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-11-28 21:03:51 +0200

    Fixing code functionality

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,14 +9,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	if (*head != NULL)
+	if (head != NULL)
 	{
 		while (*head != NULL)
 		{
-			tmp = *head;
-			(*head) = (*head)->next;
-			free(tmp);
+			tmp = (*head)->next;
+			free(*head);
+			*head = tmp;
 		}
-		*head = NULL;
+		head = NULL;
 	}
 }
