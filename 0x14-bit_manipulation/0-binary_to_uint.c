c72315eb3d1d8060ab13d99ffc5c7b20626ce52b
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-12-04 19:08:09 +0200

    Program that converts binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+  * binary_to_uint - convert a binary number to an unsigned int
+  * @b - pointer to the string containing 0's and 1's
+  * Return: the converted number, or 0 if
+  * there's a char diferent from 0 and 1
+  * b is NULL
+  */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int convertedNumber = 0;
+	int len, i, decVal = 1;
+
+	if (b == NULL)
+		return (0);
+
+	len = strlen(b);
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (b[i] == '1')
+			convertedNumber += (1 * decVal);
+		if (b[i] != '1' && b[i] != '0')
+			return (0);
+		decVal = decVal * 2;
+	}
+	return (convertedNumber);
+}
+
+int _pow(int a, int b)
+{
+	int i, p;
+
+	p = 1;
+	if (b == 0)
+		return (1);
+	for (i = 0; i < b; i++)
+		p *= a;
+	return (p);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,4 +1,6 @@
 #ifndef _main_h_
 #define _main_h_
 int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+int _pow(int a, int b);
 #endif
