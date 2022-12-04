01b847ec0e2cf6a3b81c226b4d15c0370704ef34
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-12-04 20:18:59 +0200

    Add print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+  * print_binary - prints a decimal number in a binary form
+  * @n: the decimal number
+  * Return: nothing
+  */
+void print_binary(unsigned long int n)
+{
+}
