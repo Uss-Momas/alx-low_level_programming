c58458a4395668d25229a91ca179598f1ec98569
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-12-12 22:31:48 +0200

    Fixing check 3

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		if (text_content == NULL)
 			return (1);
 
-		w_bytes = write(fd, text_content, strlen(text_content) + 1);
+		w_bytes = write(fd, text_content, strlen(text_content));
 		if (w_bytes == -1)
 			return (-1);
 		close(fd);
