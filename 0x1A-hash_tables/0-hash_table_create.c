23d30733a724c1986e8168f5461e216116265d23
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-02-02 20:40:02 +0200

    Adding calloc for the array of the hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,5 +12,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 
+	table->size = size;
+	table->array = calloc(table->size, sizeof(hash_node_t *));
+
+	if (table->array == NULL)
+		return (NULL);
 	return (table);
 }
