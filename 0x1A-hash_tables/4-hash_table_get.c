e6f56832b0ba59801a6d53d85f764e63fba657a0
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-02-03 16:35:30 +0200

    Modificating the function

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,9 +12,16 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *current_node;
 	unsigned long int index;
 
+	if (key == NULL || ht == NULL)
+		return (NULL);
+
 	index = key_index((unsigned char *) key, ht->size);
 	current_node = ht->array[index];
-	if (current_node == NULL)
-		return (NULL);
-	return (current_node->value);
+	while (current_node != NULL)
+	{
+		if (strcmp(current_node->key, key) == 0)
+			return (current_node->value);
+		current_node = current_node->next;
+	}
+	return (NULL);
 }
