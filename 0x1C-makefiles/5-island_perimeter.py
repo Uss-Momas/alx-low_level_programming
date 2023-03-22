29933a4070eff872310884a0592617f62a0a8507
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-03-22 23:12:33 +0200

    Initial structure

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""island_perimeter module"""
+
+def island_perimeter(grid):
+    """island_perimeter function
+    calculates the perimeter of a island based on it's grid
+
+    grid:
+        - list of list of integers
+    """
+    return 0
